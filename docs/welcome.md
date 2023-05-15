5206c7fa289ed532b5ec52c5cda4d26d94c25f8d
Colin <colin_teahan@yahoo.com>
2023-05-15 12:07:18 -0700

    updating the code-styling

--- docs/welcome.md
+++ docs/welcome.md
@@ -9,4 +9,21 @@ This site is a collection of everything & everything I've found on the web!
 
 ```javascript
 console.log("hello, world!");
+
+const routes = {
+  about: "/about",
+  tutorials: "/tutorials",
+};
+
+function toLearnMore(page) {
+  const route = routes[page];
+}
+```
+
+Testing the different types of code
+
+```bash
+git add .
+git commit -m "add syntax highlighting"
+git push
 ```
--- src/components/code-block.tsx
+++ src/components/code-block.tsx
@@ -1,16 +1,35 @@
 'use client'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import * as styles from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+// import * as styles from 'react-syntax-highlighter/dist/esm/styles/hljs'
+
+/**
+ * Prism themes:
+ *  - atomDark
+ *  - darcula   #1
+ *  - dracula   #2
+ */
 
 type CodeBlockProps = {
   language: string;
   children: string;
 }
 
-export const CodeBlock = ({ children, language = 'javascript' }: CodeBlockProps) => {
+const customStyle = {
+  backgroundColor: 'rgba(255, 255, 255, 0.05)',
+  fontSize: '15px',
+}
+
+export const CodeBlock = (props: CodeBlockProps) => {
+
+  console.log(props)
+
+  const { children, language } = props
+
   return (
-    <SyntaxHighlighter language={language} style={dark}>
+    <SyntaxHighlighter language={language} style={styles.okaidia} className={'code-block'} customStyle={customStyle}>
       {children}
     </SyntaxHighlighter>
   );
