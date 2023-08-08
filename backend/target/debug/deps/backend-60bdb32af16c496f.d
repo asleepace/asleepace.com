08ca7f87b31c96db3b8b7955fdd9761d3b0326db
Colin <colin_teahan@yahoo.com>
2023-08-07 21:08:35 -0700

    updated .gitignore

--- backend/target/debug/deps/backend-60bdb32af16c496f.d
+++
@@ -1,5 +0,0 @@
-/Users/asleepace/dev/asleepace.com/backend/target/debug/deps/backend-60bdb32af16c496f.rmeta: src/main.rs
-
-/Users/asleepace/dev/asleepace.com/backend/target/debug/deps/backend-60bdb32af16c496f.d: src/main.rs
-
-src/main.rs:
