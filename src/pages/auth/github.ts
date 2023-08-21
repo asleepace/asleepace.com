88fcb5e21a91960873b3d6bbd53c7dc3b0cb60fc
Colin <colin_teahan@yahoo.com>
2023-08-20 20:37:05 -0700

    updating the code

--- src/pages/auth/github.ts
+++ src/pages/auth/github.ts
@@ -8,19 +8,17 @@ export type GitHubScope = 'repo' | 'user'
 export type GitHubRedirectUri = string
 
 export const GITHUB_OAUTH_URL = 'https://github.com/login/oauth/authorize'
-export const GITHUB_CALLBACK_URL = 'http://localhost:3000/auth/callback'
+export const GITHUB_CALLBACK_URL = 'https://asleepace.com/auth/callback'
+const GITHUB_CLIENT_ID = import.meta.env.GITHUB_CLIENT_ID
 
-export const get: GitHubAPIRoute = async (context) => {
+export const get: GitHubAPIRoute = async ({ redirect }) => {
   console.log('[auth/github.ts] GitHub Oauth2 flow starting...')
-  const { request, props, redirect, params } = context
-  const { body, credentials, headers, formData } = request
-  const clientId = import.meta.env.GITHUB_CLIENT_ID
-  if (!clientId) return new Response('GITHUB_CLIENT_ID not found', { status: 500 })
+  if (!GITHUB_CLIENT_ID) return new Response('GITHUB_CLIENT_ID not found', { status: 500 })
   const redirectUri: GitHubRedirectUri = GITHUB_CALLBACK_URL
   const scope: GitHubScope = 'user'
   const oauth2Flow = new URL(GITHUB_OAUTH_URL)
-  oauth2Flow.searchParams.append('client_id', clientId)
-  oauth2Flow.searchParams.append('redirect_uri', redirectUri)
+  oauth2Flow.searchParams.append('client_id', GITHUB_CLIENT_ID)
+  // oauth2Flow.searchParams.append('redirect_uri', redirectUri)
   oauth2Flow.searchParams.append('scope', scope)
   const githubOauth2Url = oauth2Flow.toString()
   console.log('[github.ts] githubOauth2Url:', githubOauth2Url)
