20e63d4875b34488333836cb6b08c25b10466094
Colin <colin_teahan@yahoo.com>
2023-09-14 14:18:51 -0700

    src/database/metrics.ts

--- src/database/metrics.ts
+++ src/database/metrics.ts
@@ -4,3 +4,4 @@ import { Database } from 'bun:sqlite'
 const metrics = new Database('metrics.sqlite', { create: true})
 
 
+metrics.query("CREATE TABLE IF NOT EXISTS some_table (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, value INTEGER)");
