5924341cdcbe4fc28f31d593cab37a9a7d2797cc
Colin <colin_teahan@yahoo.com>
2024-07-18 07:20:43 -0700

    updating project

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from 'astro'
 
+export const prerender = false
+
 /**
  * TypeScript API
  *
@@ -18,4 +20,3 @@ export const GET: APIRoute = async ({ params, request }) => {
     })
   )
 }
-
