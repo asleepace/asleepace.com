3c10cbd12eeade9017d3ec6138fc3af2a865dc8a
Colin <colin_teahan@yahoo.com>
2024-07-18 02:22:49 -0700

    added support for bun

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+
+declare const DATABASE_URL: string
+declare const SECRET: string
+declare const SMTP_HOST: string
+declare const SMTP_PORT: 25
+declare const SMTP_USER: string
+declare const SMTP_PASSWORD: string
+declare const SMTP_FROM: string
+declare const GITHUB_CLIENT_SECRET: string
+declare const GITHUB_CLIENT_ID: string
+declare const MONGODB_URI: string
