45d95713db5ed3821274ca0ccb691dbc0fcc570b
Colin <colin_teahan@yahoo.com>
2024-12-22 15:42:38 -0800

    updated the website

--- src/components/showcase/Showcase.tsx
+++ src/components/showcase/Showcase.tsx
@@ -11,21 +11,10 @@ type ShowcaseProps = {
 function Showcase({ urls }: ShowcaseProps) {
   console.log('[Showcase] Rendering with', urls)
   return (
-    <div className="flex flex-col my-16 gap-y-8">
-      <div className="text-center flex flex-col items-center gap-y-2">
-        <Boxes className="text-7xl text-gray-300" size={96} />
-        <h1 className="text-7xl font-bold">Showcase</h1>
-        <p className="text-lg text-gray-500">
-          A collection of various projects I've done over the years.
-        </p>
-      </div>
-      <div className="w-full h-0.5 bg-gray-200" />
-      <div className="grid flex-wrap justify-between grid-cols-1 lg:grid-cols-3 gap-x-4 gap-y-4 my-8 items-center">
-        {urls.map((url, index) => (
-          <ShowcaseItem key={index} url={url} />
-        ))}
-      </div>
-      <div className="w-full h-0.5 bg-gray-200" />
+    <div className="grid flex-wrap justify-between grid-cols-1 lg:grid-cols-3 gap-x-4 gap-y-4 items-center">
+      {urls.map((url, index) => (
+        <ShowcaseItem key={index} url={url} />
+      ))}
     </div>
   )
 }
--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,17 @@
 export const SITE_TITLE = 'Asleepace'
 export const SITE_DESCRIPTION = 'a random collection of internet treasures'
 export const SITE_URL = 'https://asleepace.com'
+
+export const siteData = {
+  title: SITE_TITLE,
+  description: SITE_DESCRIPTION,
+  url: SITE_URL,
+  showcaseLinks: [
+    'https://soladex.co',
+    'https://consoledump.io',
+    'https://polyblog.net',
+    'https://patrick-teahan.com',
+    'https://lams-kitchen.com',
+    'https://dlabs.me',
+  ],
+}
