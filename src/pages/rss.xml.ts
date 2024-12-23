b20884d5ba8747e34ce3841c1fe89dfec8751d5c
Colin <colin_teahan@yahoo.com>
2024-12-22 17:33:24 -0800

    updated site

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,15 +1,15 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '@/consts'
+import { siteData } from '@/consts'
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = async (context) => {
   const posts = await getCollection('blog')
   console.log('[rss] generating rss feed:', posts)
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    site: context.site || SITE_URL,
+    title: siteData.title,
+    description: siteData.description,
+    site: context.site || siteData.url,
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}/`,
