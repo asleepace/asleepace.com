95b69cfc35d93b55b36e6dc361a8ee1590eedb34
Colin <colin_teahan@yahoo.com>
2025-06-08 04:17:28 -0700

    basic version working

--- src/components/library/defineComponent.ts
+++ src/components/library/defineComponent.ts
@@ -26,38 +26,42 @@ type InferState<S extends string> =
     ? State
     : never
 
-type PrettyState<S extends string> = S extends `"${infer Key}:${infer Value}}`
-  ? Record<Key, Value>
+type PrimitiveMap = {
+  true: boolean
+  false: boolean
+  null: null
+  undefined: undefined
+  [key: number]: number
+}
+
+type ParseValue<S extends string> = S extends keyof PrimitiveMap
+  ? PrimitiveMap[S]
+  : S
+
+type ParseState<S extends string> = S extends `{${infer Key}:${infer Value}}`
+  ? Record<Key, ParseValue<Value>>
   : never
+
+type StateString<T extends string> = ParseState<Unquote<InferState<T>>>
 
 type Test1 = InferState<`<rx-side-bar data-state='{"open":true}'>`>
 type Test2 = Unquote<Test1>
+type Test3 = ParseState<Test2>
 
 export interface RxInstance<T> extends HTMLElement {
   htmlString: string
   shadowRoot: ShadowRoot
   state: T
-  handlers: ((this, state: T) => any)[]
+  handlers: ((state: T) => any)[]
   attach(this, method: (this, state: T) => void): RxMethodHandle
 }
 
-export interface RxConfig<T extends string> {
-  tagName: string
-  props: T
-  render(this: RxInstance<T>, state: T): string
-}
-
 const findInStr = (str: string, regex: RegExp) => {
-  return str.match(regex)?.at(0)
+  return str.match(regex)?.at(1)
 }
 
-// get the data-tag attribute
-const findDataTag = (htmlString: string) =>
-  findInStr(htmlString, /data-tag="([^"]*)"/)
-
-// get the element tag name
-const findHtmlTag = (htmlString: string) =>
-  findInStr(htmlString, /<[a-zA-Z][^>]/)
+const findDataState = (htmlString: string) =>
+  findInStr(htmlString, /\bdata-state\s*=\s*(['"])((?:\\.|(?!\1)[^\\])*?)\1/)
 
 // get all html attributes
 const findAllAttr = (htmlString: string) => {
@@ -67,36 +71,36 @@ const findAllAttr = (htmlString: string) => {
   return [...matches]
 }
 
-export function defineComponent<
-  HTMLString extends string,
-  State extends Unquote<InferState<HTMLString>>
->(
-  htmlString: HTMLString,
-  handler: (instance: RxInstance<State>) => any | void
+export function defineComponent<S extends string, T extends StateString<S>>(
+  htmlString: S,
+  handler: (instance: RxInstance<T>) => any | void
 ) {
   const attributes = findAllAttr(htmlString)
   const tagName = attributes.at(0)?.at(0)
   if (!tagName) throw new Error('Missing tag name:' + htmlString)
 
   // define custom html element
-  const RxElement = class
-    extends HTMLElement
-    implements RxInstance<HTMLString>
-  {
+  const RxElement = class extends HTMLElement implements RxInstance<T> {
     static readonly observedAttributes = [...attributes]
     static {
       window.customElements.define(tagName, this)
     }
-    public htmlString: HTMLString
+    public htmlString: S
     public shadowRoot: ShadowRoot
-    public state: State
-    public handlers = []
-    // public htmlString = htmlString
+    public state: T = {} as T
+    public handlers: ((this, state: T) => any)[] = []
+    // public htmlString = htmlStrintg
     constructor() {
       super()
       this.shadowRoot = this.attachShadow({ mode: 'open' })
       this.shadowRoot.innerHTML = '<slot />'
-      this.state = {} as State
+      // const initialData = findDataState(htmlString)
+
+      const initialData = this.getAttribute('data-state')
+      if (initialData) {
+        eval(`this.state = ${initialData}`)
+        console.log({ initialData, state: this.state })
+      }
     }
     public self() {
       return 'this.getRootNode().host'
@@ -107,17 +111,16 @@ export function defineComponent<
       const handleId = `this.getRootNode().host.handlers[${methodId}]()`
       return handleId as RxMethodHandle
     }
-    public setState(nextState: Partial<State>) {
-      console.log('set state')
-      // this.state = rx.props.safeParse({ ...this.state, ...nextState })
+    public setState(nextState: Partial<T>) {
+      this.state = { ...this.state, ...nextState }
       this.render()
     }
     public render() {
       this.shadowRoot.innerHTML = `<slot />`
     }
     public connectedCallback() {
-      console.log('[rxml] connected!')
-      handler.call(this, this.state)
+      console.log('[rxml] connected:', this)
+      handler.call(this, this)
     }
     public attributeChangedCallback(
       name: string,
