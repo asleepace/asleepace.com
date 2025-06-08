58401dee56da81840acd79c7b4d1a87d7a862f76
Colin <colin_teahan@yahoo.com>
2025-06-08 06:54:15 -0700

    added custom directive

--- src/components/library/defineComponent.ts
+++ src/components/library/defineComponent.ts
@@ -1,6 +1,3 @@
-import { z, type TypeOf } from 'astro:schema'
-import { type Prettify } from './types'
-
 declare const BrandSymbol: unique symbol
 
 type RxMethodHandle = string & {
@@ -12,12 +9,42 @@ type RxHandlers<T> = ((this: ThisType<RxInstance<T>>, state: T) => any)[]
 export interface RxInstance<T> extends HTMLElement {
   shadowRoot: ShadowRoot
   state: T
-  handlers: ((state: T) => any)[]
-  attach(
-    this: RxInstance<T>,
-    method: (this: RxInstance<T>, state: T) => void
-  ): RxMethodHandle
+  self(): string
+  setState(next: Partial<T>): void
+  render(): void
 }
+
+const getRxAttrs = (elem: HTMLElement): Attr[] => {
+  return [...elem.attributes].filter((attr) => attr.name.startsWith('rx-'))
+}
+
+function collectBoundChildren(
+  child: HTMLElement | undefined,
+  found: Map<string, HTMLElement[]> = new Map()
+) {
+  if (!child) return found
+  child.childNodes.forEach((next) => {
+    if (next.nodeType === Node.ELEMENT_NODE) {
+      const child = next as HTMLElement
+      const attrs = getRxAttrs(child)
+      if (!attrs.length) return
+      attrs.forEach((attr) => {
+        const current = found.get(attr.name) || ([] as HTMLElement[])
+        found.set(attr.name, [...current, child])
+      })
+    }
+  })
+  return found
+}
+
+const attachProxy = <T extends {}>(state: T, didUpdate?: (state: T) => void) =>
+  new Proxy(state, {
+    set(target, key, value, recv) {
+      const output = Reflect.set(target, key, value, recv)
+      didUpdate?.(target)
+      return output
+    },
+  })
 
 export function defineComponent<T extends {}>({
   tagName,
@@ -31,44 +58,51 @@ export function defineComponent<T extends {}>({
   const RxElement = class extends HTMLElement implements RxInstance<T> {
     static readonly observedAttributes = Object.keys(initialState)
     static {
-      window.customElements.define(tagName, this)
+      // automatically register the element
+      if (typeof window !== 'undefined') {
+        window.customElements.define(tagName, this)
+      }
     }
     public shadowRoot: ShadowRoot
-    public state: T = initialState
     public handlers: RxHandlers<T> = []
+    public state: T = initialState
     constructor() {
       super()
       this.shadowRoot = this.attachShadow({ mode: 'open' })
       this.shadowRoot.innerHTML = '<slot />'
-
-      const onSetAttribute = (nextState: Partial<T>) => {
-        const combined = { ...this.state, ...nextState }
-      }
-
-      this.state = new Proxy(initialState, {
-        set(target, prop, value, recv) {
-          onSetAttribute({ [prop]: value } as any)
-          return Reflect.set(target, prop, value, recv)
-        },
-      })
     }
     public self() {
       return 'this.getRootNode().host'
     }
-    public attach(
-      method: (this: ThisType<RxInstance<T>>, state: T) => void
-    ): RxMethodHandle {
-      const methodId = this.handlers.length
-      this.handlers.push(() => method.call(this, this.state))
-      const handleId = `this.getRootNode().host.handlers[${methodId}]()`
-      return handleId as RxMethodHandle
+    public setState(next: Partial<T>) {
+      Object.assign(this.state, next) // triggers proxy
     }
     public render() {
       this.shadowRoot.innerHTML = `<slot />`
+      this.updateStateAttribute()
+      this.updateBoundChildren()
+    }
+    public updateStateAttribute() {
+      this.setAttribute('data-state', JSON.stringify(this.state))
+    }
+    public updateBoundChildren() {
+      collectBoundChildren(this)
+        .entries()
+        .forEach(([boundAttr, children]) => {
+          const current = this.getBoundAttribute(boundAttr)
+          children.forEach((ch) => ch.setAttribute(boundAttr, current))
+        })
+    }
+    public getBoundAttribute(boundAttr: string) {
+      return eval(this.getAttribute(boundAttr) ?? 'undefined')
     }
     public connectedCallback() {
-      console.log('[rxml] connected:', this)
+      // attach the proxy to the state
+      this.state = attachProxy(initialState, () => this.render())
+      // trigger callback method of setup
       attach.call(this, this.state)
+      // update all reactive-child
+      this.render()
     }
     public attributeChangedCallback(
       name: string,
