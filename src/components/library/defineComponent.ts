e4085ce16b1a424741399af81ea985d67aaa2eb0
Colin <colin_teahan@yahoo.com>
2025-06-14 12:01:42 -0700

    added new article

--- src/components/library/defineComponent.ts
+++ src/components/library/defineComponent.ts
@@ -1,3 +1,5 @@
+import { Try } from '@asleepace/try'
+
 type RxHandlers<T> = ((this: ThisType<RxInstance<T>>, state: T) => any)[]
 
 export interface RxInstance<T> extends HTMLElement {
@@ -32,14 +34,118 @@ function collectBoundChildren(
   return found
 }
 
-const attachProxy = <T extends {}>(state: T, didUpdate?: (state: T) => void) =>
+const attachProxy = <T extends {}>(state: T, didUpdate: (state: T) => void) =>
   new Proxy(state, {
     set(target, key, value, recv) {
       const output = Reflect.set(target, key, value, recv)
-      didUpdate?.(target)
+      didUpdate({ ...target, [key]: value })
       return output
     },
   })
+
+function attributeCoder<T extends {}, K extends keyof T>(
+  this: HTMLElement,
+  initialState: T
+) {
+  const keys = Object.keys(initialState) as K[]
+  const toDataKey = (key: string) => `data-${key}`
+  const dataKeys = keys.filter((key) => typeof key === 'string').map(toDataKey)
+  const safeParse = (obj: string) => Try.catch(() => JSON.parse(obj))[0]
+  const safeStringify = <T extends {}>(obj: T) =>
+    Try.catch(() => JSON.stringify(obj))[0]
+
+  const parseBoolean = (str: string) => {
+    switch (str) {
+      case 'true':
+      case '1':
+        return true
+      default:
+        return false
+    }
+  }
+
+  const decodeAttribute = (
+    dataKey: string,
+    value: string | null
+  ): [key: K, value: any] => {
+    const key = dataKey.slice(5) as K // slice off "data-"
+    if (!value) return [key, undefined] as const
+    if (value === 'undefined') return [key, undefined] as const
+    if (value === 'null') return [key, undefined] as const
+    switch (typeof initialState[key]) {
+      case 'string':
+        return [key, value]
+      case 'number':
+        return [key, Number(value)]
+      case 'undefined':
+        return [key, undefined]
+      case 'bigint':
+        return [key, BigInt(value)]
+      case 'symbol':
+        return [key, initialState[key]] // return og value for symbol
+      case 'function':
+        return [key, initialState[key]] // return og reference for functions
+      case 'boolean':
+        return [key, parseBoolean(value)]
+      case 'object':
+        return [key, safeParse(value)]
+      default:
+        return [key, undefined]
+    }
+  }
+
+  const encodeAttribute = <T>(value: T) => {
+    switch (typeof value) {
+      case 'string':
+        return value
+      case 'number':
+        return String(value)
+      case 'bigint':
+        return String(value)
+      case 'boolean':
+        return String(value)
+      case 'symbol':
+        return value.toString()
+      case 'function':
+        return `${value.name}()`
+      case 'undefined':
+        return 'undefined'
+      case 'object': {
+        if (!value) return 'null'
+        return safeStringify(value)
+      }
+    }
+  }
+
+  return {
+    keys: () => keys,
+    encode: <T extends {}>(data: T) => {
+      const nextState = Object.entries(data).map(([key, value]) => {
+        const encodedValue = encodeAttribute(value)
+        if (!encodedValue) {
+          this.removeAttribute(toDataKey(key))
+          return [key, value]
+        } else {
+          this.setAttribute(toDataKey(key), encodedValue)
+          return [key, undefined]
+        }
+      })
+
+      return Object.fromEntries(nextState)
+    },
+    decode: <T>() => {
+      return Object.fromEntries(
+        keys.map((key) => {
+          const dataKey = toDataKey(key as string)
+          const value = this.getAttribute(dataKey)
+          return decodeAttribute(dataKey, value)
+        })
+      ) as T
+    },
+  }
+}
+
+type AttributeCoder = ReturnType<typeof attributeCoder>
 
 export function defineComponent<T extends {}>({
   tagName,
@@ -68,20 +174,37 @@ export function defineComponent<T extends {}>({
     }
     public shadowRoot: ShadowRoot
     public handlers: RxHandlers<T> = []
-    public state: T = initialState
+    public coder: AttributeCoder
+    public attach: (this: RxInstance<T>, state: T) => any | void
+
     constructor() {
       super()
       this.shadowRoot = this.attachShadow({ mode: 'open' })
       this.shadowRoot.innerHTML = '<slot />'
+      this.coder = attributeCoder.call(this, initialState)
+      this.coder.encode(initialState)
+      this.coder.decode()
+      this.attach = attach.bind(this)
       this.updateBoundChildren()
     }
+
+    public get state(): T {
+      return this.coder.decode.call(this)
+    }
+
+    public set state(nextState: T) {
+      this.coder.encode.call(this, nextState)
+    }
+
     public self() {
       return 'this.getRootNode().host'
     }
     public setState(next: Partial<T>) {
-      Object.assign(this.state, next) // triggers proxy
+      // Object.assign(this.state, next) // triggers proxy
+      this.state = { ...this.state, ...next }
     }
     public render() {
+      console.log('[component] render called!')
       this.shadowRoot.innerHTML = `<slot />`
       this.updateStateAttribute()
       this.updateBoundChildren()
@@ -94,15 +217,7 @@ export function defineComponent<T extends {}>({
         .entries()
         .forEach(([boundAttr, children]) => {
           const current = this.getBoundAttribute(boundAttr)
-          console.log({ boundAttr, current, children })
           children.forEach((ch) => {
-            console.log(
-              'updated child:',
-              ch,
-              boundAttr,
-              current,
-              ch.getAttributeNames()
-            )
             ch.setAttribute(boundAttr, current)
           })
         })
@@ -111,10 +226,11 @@ export function defineComponent<T extends {}>({
       return eval(this.getAttribute(boundAttr) ?? 'undefined')
     }
     public connectedCallback() {
+      console.log('[component] connected!', this)
       // attach the proxy to the state
-      this.state = attachProxy(initialState, () => this.render())
+      // this.state = attachProxy(initialState, () => this.render())
       // trigger callback method of setup
-      attach.call(this, this.state)
+      this.attach.call(this, this.state)
       // update all reactive-child
       this.render()
       // register instance
@@ -128,6 +244,11 @@ export function defineComponent<T extends {}>({
       oldValue: string | null,
       newValue: string | null
     ) {
+      console.log('[component] attribute changed:', {
+        name,
+        oldValue,
+        newValue,
+      })
       if (!this.isConnected || oldValue === newValue) return
       if (name in this.state) {
         this.state[name] = newValue
@@ -141,4 +262,29 @@ export function defineComponent<T extends {}>({
     class: CustomComponent,
     ...shared,
   }
+}
+
+export const $define = <Elem extends HTMLElement, K extends keyof Elem>(
+  tagName: string,
+  config: Partial<Record<K, Elem[K]>> & { $state?: {} } = {}
+) => {
+  return defineComponent({
+    tagName,
+    state: {
+      ...(document.getElementsByTagName(tagName).item(0)?.attributes || {}),
+      ...(config.$state || {}),
+    },
+    attach(this: any) {
+      for (const key in config) {
+        const valueOrFunc = config[key]
+        if (!(key in this)) continue
+        switch (typeof valueOrFunc) {
+          case 'function':
+            this[key] = valueOrFunc.bind(this)
+          default:
+            this[key] = valueOrFunc
+        }
+      }
+    },
+  })
 }
---
+++ src/content/blog/export-imessage-for-ai.md
@@ -0,0 +1,160 @@
+---
+title: 'How to Export iMessage Chat History'
+description: 'How to export your iMessage chat history to a text file which can be uploaded and analyzed with Ai.'
+pubDate: 'June 14 2025'
+heroImage: '/images/try-catch-cover.jpg'
+slug: 'ai-imessage'
+---
+
+The other day the group chat my friends and I use to talk about stocks, trading and investing devolved into pure political chaos. Desperate to find the culprit who kept steering the conversation towards politics, we turned to Ai.
+
+Our first attempts consisted of taking screenshots and uploading them individually, but this proved to be painfully slow and missing a lof of context, such as was someone responding to a question asked by another member about politics or was the conversation about current events which could affect the markets.
+
+### How to access iMessage on MacOS
+
+If your MacOS is connected with your iCloud account, the all your iMessages are actually saved in an sqlite database. To check if this exists run the following:
+
+```sh
+ls ~/Library/Messages/chat.db
+```
+
+You should see somthing like `/Users/asleepace/Library/Messages/chat.db` as the output. This is your iMessage sqlite database! In order to open you may need to give your Terminal "Full disk access" permissions:
+
+<img src="/images/full-disk-access.png" />
+
+After restarting the terminal you should then be able to open the file with the `sqlite3` command (when run as `sudo`).
+
+```sh
+sudo sqlite3 ~/Library/Messages/chat.db
+```
+
+From here you can execute raw SQL commands, lets view what tables are available:
+
+```sql
+. tables
+```
+
+This should show you a list like the following:
+
+```txt
+_SqliteDatabaseProperties
+attachment
+chat
+chat_handle_join
+chat_message_join
+chat_recoverable_message_join
+deleted_messages
+handle
+kvtable
+message
+message_attachment_join
+message_processing_task
+recoverable_message_part
+scheduled_messages_pending_cloudkit_delete
+sync_deleted_attachments
+sync_deleted_chats
+sync_deleted_messages
+unsynced_removed_recoverable_messages
+```
+
+Now let's find that specific group chat, in my case it's name is `'Money Gang'`
+
+```sql
+SELECT
+    c.chat_identifier,
+    c.display_name,
+    COUNT(m.ROWID) as message_count,
+    datetime(MAX(m.date)/1000000000 + strftime("%s", "2001-01-01"), "unixepoch", "localtime") as last_message_date
+FROM chat c
+LEFT JOIN chat_message_join cmj ON c.ROWID = cmj.chat_id
+LEFT JOIN message m ON cmj.message_id = m.ROWID
+WHERE c.display_name IN ('Money Gang')  -- note "Moeny Gang" is the name of our chat
+GROUP BY c.ROWID
+ORDER BY MAX(m.date) DESC;
+```
+
+Since this group chat has been ongoing for quite some time, I actually found several instances:
+
+```txt
+chat449416128167948589|Money Gang|17818|2025-06-14 10:20:16
+chat449416128167948589|Money Gang|6|2025-06-09 16:07:01
+chat145145727747807125|Money Gang|1053|2025-05-16 13:10:17
+```
+
+We can check how many messages are in each instance with the following passing in the specific id, in my case `'chat449416128167948589'`:
+
+```sql
+SELECT COUNT(*) as total_messages
+FROM message m
+JOIN chat_message_join cmj ON m.ROWID = cmj.message_id
+JOIN chat c ON cmj.chat_id = c.ROWID
+WHERE c.chat_identifier = 'chat449416128167948589'
+    AND m.text IS NOT NULL
+    AND m.text != ''
+    AND m.text != '￼'
+    AND LENGTH(TRIM(m.text)) > 0;
+```
+
+If you want to extract all the messages from a specific chat we can do the following:
+
+```sql
+SELECT
+    h.id as sender,
+    COALESCE(h.id, 'Me') as sender,
+    COALESCE(m.text, '[No text]') as message,
+    datetime(m.date/1000000000 + strftime("%s", "2025-01-01"), "unixepoch", "localtime") as date
+FROM message m
+JOIN chat_message_join cmj ON m.ROWID = cmj.message_id
+JOIN chat c ON cmj.chat_id = c.ROWID
+LEFT JOIN handle h ON m.handle_id = h.ROWID
+WHERE c.chat_identifier = 'chat449416128167948589'
+ORDER BY m.date DESC
+LIMIT 100;
+```
+
+Or if you want to extract all messages from all group chats with that display name:
+
+```sql
+SELECT
+  c.chat_identifier,
+  COALESCE(h.id, 'Me') as sender,
+   m.text as message,
+   datetime(m.date/1000000000 + strftime('%s', '2001-01-01'), 'unixepoch', 'localtime') as date
+FROM message m
+JOIN chat_message_join cmj ON m.ROWID = cmj.message_id
+JOIN chat c ON cmj.chat_id = c.ROWID
+LEFT JOIN handle h ON m.handle_id = h.ROWID
+WHERE c.display_name = 'Money Gang'
+     AND m.text IS NOT NULL
+     AND m.text != ''
+     AND m.text != '￼'
+     AND LENGTH(TRIM(m.text)) > 0
+ORDER BY m.date ASC;
+```
+
+### Exporting to a Text File
+
+Since we wanted to use Ai to read the conversation and determine who was the culprit, the next step was to export the combined conversations into a single text file which could be uploaded to Claude.
+
+The following shell command will coalesce all `'Money Gang'` chat threads into a single text file `chat.txt`
+
+```sh
+#!/bin/zsh
+sudo sqlite3 ~/Library/Messages/chat.db "SELECT
+    c.chat_identifier,
+    COALESCE(h.id, 'Me') as sender,
+    m.text as message,
+    datetime(m.date/1000000000 + strftime('%s', '2001-01-01'), 'unixepoch', 'localtime') as date
+FROM message m
+JOIN chat_message_join cmj ON m.ROWID = cmj.message_id
+JOIN chat c ON cmj.chat_id = c.ROWID
+LEFT JOIN handle h ON m.handle_id = h.ROWID
+WHERE c.display_name = 'Money Gang'
+    AND m.text IS NOT NULL
+    AND m.text != ''
+    AND m.text != '￼'
+    AND LENGTH(TRIM(m.text)) > 0
+ORDER BY m.date ASC;" >> chat.txt
+```
+
+The file should now be in the directory where you ran the command.
