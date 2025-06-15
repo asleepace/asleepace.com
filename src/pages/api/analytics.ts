9164c4ceb3429914a6b8e1492a8417816d8fb2e3
Colin <colin_teahan@yahoo.com>
2025-06-15 02:25:13 -0700

    merging latest code

---
+++ src/pages/api/analytics.ts
@@ -0,0 +1,47 @@
+import type { APIRoute } from 'astro'
+import { endpoint } from '@/pages/api'
+import { Analytics } from '@/db'
+import { http } from '@/lib/web'
+import { getIpAddressFromHeaders } from '@/lib/utils/ipAddress'
+
+export const prerender = false
+
+/**
+ * POST /api/analytics
+ *
+ * This endpoint is used to track analytics for the website.
+ *
+ */
+export const POST: APIRoute = endpoint(async ({ request }) => {
+  const { headers } = request
+
+  const ipAddress = getIpAddressFromHeaders(headers)
+  const userAgent = headers.get('user-agent')
+  const sessionId = headers.get('session')
+  const referrer = headers.get('referer')
+
+  Analytics.track({
+    path: request.url,
+    ipAddress,
+    userAgent,
+    sessionId,
+    referrer,
+  })
+
+  return http.success({ ipAddress })
+})
+
+/**
+ * GET /api/analytics
+ *
+ * This endpoint is used to fetch analytics data from the database.
+ *
+ */
+export const GET: APIRoute = endpoint(async ({ request, locals }) => {
+  if (!locals.isLoggedIn) return http.failure(401, 'Unauthorized')
+  const { searchParams } = await http.parse(request)
+  const limit = parseInt(searchParams['limit']) ?? 100
+  const offset = parseInt(searchParams['offset']) ?? 0
+  const data = Analytics.fetchAnalytics(limit, offset)
+  return http.success({ data, limit, offset })
+})
---
+++ src/pages/api/proxy.ts
@@ -0,0 +1,77 @@
+import type { APIRoute } from 'astro'
+import { http } from '@/lib/web'
+import { endpoint, Exception } from '@/pages/api'
+import { isResponse, isURL, isSet } from '@/lib/utils'
+
+class UserAgents {
+  static MICROSOFT =
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+  static APPLE =
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+}
+
+export const prerender = false
+
+export type ApiProxyResponse = {
+  status: number
+  contentType: string
+  data: string
+}
+
+/**
+ *
+ *  GET /api/proxy?uri={uri}
+ *
+ *  This endpoint will decode the URI parameter and fetch the resource, returning
+ *  the response to the client.
+ *
+ *  @response {ApiProxyResponse}
+ *
+ */
+export const GET: APIRoute = endpoint(async ({ request }) => {
+  const { searchParams } = await http.parse(request)
+  const { uri } = searchParams
+
+  Exception.assert(isSet(uri), 400, 'Missing URI parameter')
+  Exception.assert(isURL(uri), 400, 'Invalid URI parameter')
+
+  const spoofedHost = new URL(uri).host
+
+  // Pass through headers to the remote server
+  const headers = new Headers(request.headers)
+  headers.has('user-agent') || headers.set('user-agent', UserAgents.APPLE)
+  headers.has('accept') || headers.set('accept', '*/*')
+
+  // Remove potentially identifying headers
+  headers.delete('cookie')
+  headers.delete('accept-language')
+  headers.delete('sec-ch-ua')
+  headers.delete('sec-ch-ua-mobile')
+  headers.delete('sec-ch-ua-platform')
+
+  // Remove forwarding headers
+  headers.delete('x-forwarded-for')
+  headers.delete('x-forwarded-host')
+  headers.delete('x-forwarded-proto')
+  headers.delete('forwarded')
+  headers.delete('via')
+
+  // Disable compression to simplify response handling
+  headers.set('accept-encoding', 'identity')
+
+  // Spoof headers to prevent blocking
+  headers.set('x-forwarded-host', spoofedHost)
+  headers.set('x-forwarded-for', '127.0.0.1')
+  headers.set('host', spoofedHost)
+  headers.set('referer', uri)
+  headers.set('origin', uri)
+
+  const response = await fetch(uri, {
+    method: 'GET',
+    headers,
+  }).catch((e) => e as Error)
+
+  Exception.assert(isResponse(response), 500, String(response))
+
+  return response.clone()
+})
