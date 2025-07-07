7e61d1db310c63efad0a5bfd69f43250e1e6a3fc
Colin <colin_teahan@yahoo.com>
2025-07-07 12:49:06 -0700

    check if user is admin before showing analytics

--- src/pages/api/analytics.ts
+++ src/pages/api/analytics.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from 'astro'
 import { endpoint } from '@/pages/api'
-import { Analytics } from '@/db'
+import { Analytics, Users } from '@/db'
 import { http } from '@/lib/web'
 import { getIpAddressFromHeaders } from '@/lib/backend/ipAddress'
 
@@ -33,7 +33,7 @@ export const POST: APIRoute = endpoint(async ({ request }) => {
  */
 export const GET: APIRoute = endpoint(async ({ request, locals }) => {
   if (!locals.isLoggedIn) return http.failure(401, 'Unauthorized')
-  if (!locals.user) return http.failure(403, 'Forbidden')
+  if (!locals.user || !Users.isAdmin(locals.user)) return http.failure(403, 'Forbidden')
   const { searchParams } = await http.parse(request)
   const daysParam = searchParams['days'] ? parseInt(searchParams['days']) : 30
   const days = isNaN(daysParam) ? 30 : daysParam
