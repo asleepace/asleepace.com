67ac958e213519c662d49e7480d91d697f3a49a1
Colin <colin_teahan@yahoo.com>
2025-12-02 10:12:37 -0800

    removed legacy types

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,36 +6,6 @@ import type { User, Metric } from './db'
 
 export type PageMetrics = Metric
 
-interface ImportMetaEnv {
-  // Postgres
-  POSTGRES_CONNECTION_STRING: string
-  POSTGRES_PASSWORD: string
-  POSTGRES_USERNAME: string
-  POSTGRES_DATABASE: string
-  POSTGRES_HOST: string
-  POSTGRES_PORT: string
-  // Mongo (deprecated)
-  MONGODB_URI: string
-  // SMTP
-  SMTP_HOST: string
-  SMTP_PORT: string
-  SMTP_USER: string
-  SMTP_PASSWORD: string
-  SMTP_FROM: string
-  // API Keys
-  GROK_API_KEY: string
-  // System
-  CHROME_EXECUTABLE_PATH: string
-  CHROME_COOKIE_REDDIT: string
-  // WebAuthN
-  WEBAUTHN_RP_ID: string
-  WEBAUTHN_RP_ORIGIN: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
-
 declare global {
   type ToString = { toString(): string }
   type Blog = CollectionEntry<'blog'>
