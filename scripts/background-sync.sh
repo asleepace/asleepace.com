0821e7d1b2e0094d1d54f45f520ab1fe910a253c
Colin <colin_teahan@yahoo.com>
2025-12-03 11:15:54 -0800

    updated the script

--- scripts/background-sync.sh
+++ scripts/background-sync.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# DESCRIPTION:
+#
+# Trigger this script to run with a pm2 Chron job:
+#
+#   pm2 start scripts/background-sync.sh \
+#     --name stock-background-sync \
+#     --cron "*/15 * * * *" \
+#     --no-autorestart
+#
+# or use the npm script
+#
+#   bun run background:jobs
+#
+
 # Background sync script - hits the API endpoint every 15 mins
 curl -s https://asleepace.com/api/stocks/background-sync
 
