41936e4797e1662c49f9bfcc865d911350406beb
Colin <colin_teahan@yahoo.com>
2025-12-03 11:23:51 -0800

    fixed a typo

--- scripts/background-sync.sh
+++ scripts/background-sync.sh
@@ -2,7 +2,7 @@
 
 # DESCRIPTION:
 #
-# Trigger this script to run with a pm2 Chron job:
+# Trigger this script to run with a pm2 cron job:
 #
 #   pm2 start scripts/background-sync.sh \
 #     --name stock-background-sync \
